0c0aba10623001619d6a3bdc9e9ce9275fc1bf0e
Jonathan <jonathan@duostudio.com.br>
2018-06-16 21:17:35 -0300

    production

--- src/providers/service/service.ts
+++ src/providers/service/service.ts
@@ -10,8 +10,8 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ServiceProvider {
 
-	public API_URL: string = 'http://localhost/Particular/plantapp-api/';
-	// public API_URL: string = 'http://freitasjonathan08.000webhostapp.com/plantapp-api/';
+	// public API_URL: string = 'http://localhost/Particular/plantapp-api/';
+	public API_URL: string = 'http://freitasjonathan08.000webhostapp.com/plantapp-api/';
 
 	constructor(public http: Http) {
 
